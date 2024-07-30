85a3b2f343fabd14f38a48b1f0821404907c579a
ChavaGood <gutterman.programmer@gmail.com>
2024-07-30 17:00:45 +0300

    end task

---
+++ communication/build/CMakeFiles/communication.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/Validation/CRCalgoritm.cpp" "CMakeFiles/communication.dir/Validation/CRCalgoritm.cpp.o" "gcc" "CMakeFiles/communication.dir/Validation/CRCalgoritm.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/Validation/CRCalgoritm_test.cpp" "CMakeFiles/communication.dir/Validation/CRCalgoritm_test.cpp.o" "gcc" "CMakeFiles/communication.dir/Validation/CRCalgoritm_test.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/Validation/Convert.cpp" "CMakeFiles/communication.dir/Validation/Convert.cpp.o" "gcc" "CMakeFiles/communication.dir/Validation/Convert.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/Validation/Convert_test.cpp" "CMakeFiles/communication.dir/Validation/Convert_test.cpp.o" "gcc" "CMakeFiles/communication.dir/Validation/Convert_test.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/Validation/D3Validation.cpp" "CMakeFiles/communication.dir/Validation/D3Validation.cpp.o" "gcc" "CMakeFiles/communication.dir/Validation/D3Validation.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/Validation/MatValidation.cpp" "CMakeFiles/communication.dir/Validation/MatValidation.cpp.o" "gcc" "CMakeFiles/communication.dir/Validation/MatValidation.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/Validation/Validation.cpp" "CMakeFiles/communication.dir/Validation/Validation.cpp.o" "gcc" "CMakeFiles/communication.dir/Validation/Validation.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/Validation/Validation_test.cpp" "CMakeFiles/communication.dir/Validation/Validation_test.cpp.o" "gcc" "CMakeFiles/communication.dir/Validation/Validation_test.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/Validation/VecValidation.cpp" "CMakeFiles/communication.dir/Validation/VecValidation.cpp.o" "gcc" "CMakeFiles/communication.dir/Validation/VecValidation.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/communication/D3Message.cpp" "CMakeFiles/communication.dir/communication/D3Message.cpp.o" "gcc" "CMakeFiles/communication.dir/communication/D3Message.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/communication/D3Message_test.cpp" "CMakeFiles/communication.dir/communication/D3Message_test.cpp.o" "gcc" "CMakeFiles/communication.dir/communication/D3Message_test.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/communication/Data.cpp" "CMakeFiles/communication.dir/communication/Data.cpp.o" "gcc" "CMakeFiles/communication.dir/communication/Data.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/communication/Data_test.cpp" "CMakeFiles/communication.dir/communication/Data_test.cpp.o" "gcc" "CMakeFiles/communication.dir/communication/Data_test.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/communication/MatMessage.cpp" "CMakeFiles/communication.dir/communication/MatMessage.cpp.o" "gcc" "CMakeFiles/communication.dir/communication/MatMessage.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/communication/MatMessage_test.cpp" "CMakeFiles/communication.dir/communication/MatMessage_test.cpp.o" "gcc" "CMakeFiles/communication.dir/communication/MatMessage_test.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/communication/Message.cpp" "CMakeFiles/communication.dir/communication/Message.cpp.o" "gcc" "CMakeFiles/communication.dir/communication/Message.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/communication/Meta_Data.cpp" "CMakeFiles/communication.dir/communication/Meta_Data.cpp.o" "gcc" "CMakeFiles/communication.dir/communication/Meta_Data.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/communication/VecMessage.cpp" "CMakeFiles/communication.dir/communication/VecMessage.cpp.o" "gcc" "CMakeFiles/communication.dir/communication/VecMessage.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/communication/VecMessage_test.cpp" "CMakeFiles/communication.dir/communication/VecMessage_test.cpp.o" "gcc" "CMakeFiles/communication.dir/communication/VecMessage_test.cpp.o.d"
+  "/mnt/c/Users/Admin/Desktop/חוי/חוי לימודים/MultiDrone/communication/communication/communication.cpp" "CMakeFiles/communication.dir/communication/communication.cpp.o" "gcc" "CMakeFiles/communication.dir/communication/communication.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
