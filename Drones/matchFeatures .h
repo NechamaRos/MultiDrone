c4caf622845da0791ee37b09efae009bca774e86
”TovaGit” <tova6302@gmail.com>
2024-08-04 17:42:41 +0300

    Remove file from version control

--- Drones/matchFeatures .h
+++ Drones/matchFeatures .h
@@ -22,13 +22,13 @@ class MatchFeaturers:public HashTable
 private:
 	std::vector<std::vector<int>> descriptors_1;
 	std::vector<std::vector<int>> descriptors_2;
-    std::vector<PointMatch>good_matches;
     std::vector<std::vector<PointMatch>> knn_matches;
 public:
+	std::vector<PointMatch>good_matches;
 	std::vector<std::pair<std::vector<int>, std::vector<int>>>keyPointPaires;
 	std::vector<std::vector<PointMatch>> knnMatch(const std::vector<std::vector<int>>& descriptors1, const std::vector<std::vector<int>>& descriptors2, int k);
 	void knnSearch(const std::vector<std::vector<int>>& descriptors1, const std::vector<std::vector<int>>& descriptors2, std::vector<std::vector<int>>& indices, std::vector<std::vector<float>>& dists, int knn);
-	//std::vector<PointMatch> matchFilter(const std::vector<std::vector<PointMatch>>& knn_matches);
+	std::vector<PointMatch> matchFilter(const std::vector<std::vector<PointMatch>>& knn_matches);
 	void findNeighbors(const std::vector<int>& vec, ResultSet& result, const std::vector<std::vector<int>>& descriptors2);
 	std::vector<std::pair<std::vector<int>, std::vector<int>>>setForHomorgraphy(std::vector<PointMatch>good_matches);
 };
