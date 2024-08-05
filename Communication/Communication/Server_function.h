698017356b8d37400e2a50c131054cdffd044d76
noaRechnitzer <noa67661@gmail.com>
2024-08-05 12:35:21 +0300

    Full multi-device communication - two-way messaging - server side

---
+++ Communication/Communication/Server_function.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <winsock2.h>
+#include <vector>
+#define DEFAULT_BUFLEN 512
+
+#pragma region regular connection
+int initialize_winsock();
+int get_server_info(const char* hostname, struct addrinfo* hints, struct addrinfo** servinfo);
+int bind_to_first_available_socket(int& sockfd, struct addrinfo* servinfo);
+int start_listening(int& sockfd);
+#pragma endregion
+// Add a new file descriptor to the set
+void add_to_activeConnectionPollfds(struct pollfd* activeConnectionPollfds[], int newfd, int* fd_count, int* fd_size);
+
+// Remove an index from the set
+void del_from_activeConnectionPollfds(struct pollfd activeConnectionPollfds[], int i, int* fd_count);
+
+//while(true)
+int define_clients_sockets_and_poll(std::vector<int>& clientSockets, WSAPOLLFD fds[FD_SETSIZE]);
+int check_about_new_client_connection(int& ListenSocket, WSAPOLLFD fds[FD_SETSIZE], std::vector<int>& clientSockets);
+void accept_message(std::vector<int>& clientSockets, int& i, char recvbuf[DEFAULT_BUFLEN], int& recvbuflen);
+void checking_incoming_data_for_each_client(WSAPOLLFD fds[FD_SETSIZE], std::vector<int>& clientSockets, char recvbuf[DEFAULT_BUFLEN], int& recvbuflen);
+void cleanup(int ListenSocket);
+
+void send_message_to_all(int& sockfd, char* message);
+int send_message_to_drone(int clientSocket);
---
+++ Communication/Communication/Server_test.cpp
@@ -0,0 +1,60 @@
+#include "doctest.h"
+#include "Server_function.h"
+#ifndef _ws2tcpip
+#define ws2tcpip
+#include <ws2tcpip.h>
+#endif // !_ws2tcpip
+
+
+int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+struct addrinfo hints, * servinfo;
+int ListenSocket = INVALID_SOCKET;
+char recvbuf[DEFAULT_BUFLEN];
+int recvbuflen = DEFAULT_BUFLEN;
+
+TEST_CASE("initialize_winsock") {
+	int x = initialize_winsock();
+	printf("initialize_winsock=%d\n", x);
+	CHECK(x == 0);
+}
+
+TEST_CASE("get_server_info") {
+	int x = get_server_info(" ", &hints, &servinfo);
+	printf("get_server_info=%d\n", x);
+	CHECK(x == 0);
+}
+
+TEST_CASE("bind_to_first_available_socket") {
+	int x = bind_to_first_available_socket(sockfd, servinfo);
+	printf("bind_to_first_available_socket=%d\n", x);
+	CHECK(x == 0);
+}
+
+TEST_CASE("start_listening") {
+	int x = start_listening(sockfd);
+	printf("start_listening=%d\n", x);
+	CHECK(x == 0);
+}
+
+TEST_CASE("the central while") {
+	std::vector<int> clientSockets;
+	WSAPOLLFD fds[FD_SETSIZE];
+	fds[0].fd = sockfd;
+	fds[0].events = POLLRDNORM;
+	int i = 0;
+	while (true)
+	{
+		//printf("%d", ++i);
+		int result = define_clients_sockets_and_poll(clientSockets, fds);
+		if (result == 1) break;
+		result = check_about_new_client_connection(sockfd, fds, clientSockets);
+		if (result == 1) continue;
+		checking_incoming_data_for_each_client(fds, clientSockets, recvbuf, recvbuflen);
+	}
+
+}
+
+TEST_CASE("clean up") {
+	cleanup(sockfd);
+	printf("close_all\n");
+}
