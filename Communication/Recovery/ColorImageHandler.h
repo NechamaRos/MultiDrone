da217ea16389ea796bc3d1d9487f76d07bcc7c30
Ester German <stg56516@gmail.com>
2024-08-05 11:03:52 +0300

    Add color file

--- Communication/Recovery/ColorImageHandler.h
+++ Communication/Recovery/ColorImageHandler.h
@@ -10,7 +10,7 @@ private:
 public:
     vector<uint8_t> toOneDementionalArray(vector<vector<vector<uint8_t>>>& imageData);
     //ColorImageHandler(vector<vector<vector<uint8_t>>>& inputImage);
-    //Poly toPolynomial(GaloisField& gf) override;
+    //Poly toPolynomial(GaloisField& gf) override;   
     //void fromPolynomial(Poly& poly) override;
     //vector<vector<vector<uint8_t>>> getImageData();
 };
