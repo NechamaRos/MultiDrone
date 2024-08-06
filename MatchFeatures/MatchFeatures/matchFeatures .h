7919c684940965ed89a8858844fc0788bcdc4fa7
”TovaGit” <tova6302@gmail.com>
2024-08-06 10:32:25 +0300

    knn_matches private

--- MatchFeatures/MatchFeatures/matchFeatures .h
+++ MatchFeatures/MatchFeatures/matchFeatures .h
@@ -21,10 +21,10 @@ class MatchFeaturers :public HashTable
 private:
 	std::vector<std::vector<int>> descriptors_1;
 	std::vector<std::vector<int>> descriptors_2;
+	std::vector<std::vector<PointMatch>> knn_matches;
+
 public:
 	std::vector<PointMatch>good_matches;
-	std::vector<std::vector<PointMatch>> knn_matches;
-	std::vector<std::pair<std::vector<int>, std::vector<int>>>keyPointPaires;
 	std::vector<std::vector<PointMatch>> knnMatch(const std::vector<std::vector<int>>& descriptors1, const std::vector<std::vector<int>>& descriptors2, int k);
 	void knnSearch(const std::vector<std::vector<int>>& descriptors1, const std::vector<std::vector<int>>& descriptors2, std::vector<std::vector<int>>& indices, std::vector<std::vector<float>>& dists, int knn);
 	std::vector<PointMatch> matchFilter(const std::vector<std::vector<PointMatch>>& knn_matches);
