4f0f6256c04fae978ccad062892d55edcb382eb8
shifi325 <shifi5889@gmail.com>
2024-08-06 15:42:15 +0300

    add array

--- Storage/Disk_Mng_Master.c
+++ Storage/Disk_Mng_Master.c
@@ -15,14 +15,26 @@ void disk_mng_initialize_CB()
 void disk_mng_initialize()
 {
     disk_mng_initialize_CB();
-    stack_initialize();
-
+    stack_firstInitialize();
+    array_firstInitialize();
 }
-void stack_initialize() {
-    disk_mng_CB->diskFreeIndexesInArray=(DiskFreeIndexesInArray_t*)allocate_memory(sizeof(DiskFreeIndexesInArray_t), "Failed to allocate memory for stack ", "stack_initialize");
+//initialize on the first time we turn o the computer the stack will initialize with all the indexes.
+void stack_firstInitialize() {
+    disk_mng_CB->diskFreeIndexesInArray=(DiskFreeIndexesInArray_t*)allocate_memory(sizeof(DiskFreeIndexesInArray_t), "Failed to allocate memory for stack ", "stack_firstInitialize");
     disk_mng_CB->diskFreeIndexesInArray->top = NULL;
+    disk_mng_CB->diskFreeIndexesInArray->size = 0;
+    for (size_t i = 0; i < DISK_SIZE; i++)
+    {
+        StackNode_t* s= stackNode_create(i);
+        stack_push(s);
+    }
+}
 
-    //API call if its first time or ot
+void stack_normalInitialize() {
+    disk_mng_CB->diskFreeIndexesInArray = (DiskFreeIndexesInArray_t*)allocate_memory(sizeof(DiskFreeIndexesInArray_t), "Failed to allocate memory for stack ", "stack_normalInitialize");
+    int length = 0;
+    //disk_loadDataForInitializeDataStructers(length, 0, sizeof(int));
+    //disk_loadDataForInitializeDataStructers(disk_mng_CB->diskFreeIndexesInArray, 4, length*sizeof(int));
 }
 
 bool stack_is_empty() {
@@ -40,26 +52,54 @@ StackNode_t* stackNode_create(int index)
 
 void stack_push(StackNode_t* new_node) 
 {
+
+    disk_mng_CB->diskFreeIndexesInArray->size++;
     disk_mng_CB->diskFreeIndexesInArray->top = new_node;
 }
 
 int stack_pop() {
-    if (stack_is_empty(disk_mng_CB->diskFreeIndexesInArray)) {
+    if (stack_is_empty()) {
         return -1;
     }
     StackNode_t* temp = disk_mng_CB->diskFreeIndexesInArray->top;
     int value = temp->freeIndex;
     disk_mng_CB->diskFreeIndexesInArray->top = disk_mng_CB->diskFreeIndexesInArray->top->next;
+    disk_mng_CB->diskFreeIndexesInArray->size--;
     free(temp);
     return value;
 }
 
-int stack_top(DiskFreeIndexesInArray_t* stack) {
-    if (stack_is_empty(stack)) {
+int stack_top() {
+    if (stack_is_empty()) {
         return -1;
     }
-    return stack->top->freeIndex;
+    return disk_mng_CB->diskFreeIndexesInArray->top->freeIndex;
 }
+
+//array fuctions
+
+void array_firstInitialize() {
+    disk_mng_CB->arrayForAllMApsInformation = (ArrayInfo_t**)allocate_memory(DISK_SIZE*sizeof(ArrayInfo_t*), "Failed to allocate memory for stack ", "array_firstInitialize");
+}
+
+void array_normalInitialize() {
+    disk_mng_CB->diskFreeIndexesInArray = (ArrayInfo_t**)allocate_memory(sizeof(ArrayInfo_t*), "Failed to allocate memory for stack ", "array_normalInitialize");
+    //disk_loadDataForInitializeDataStructers(disk_mng_CB->diskFreeIndexesInArray, 4+ disk_mng_CB->diskFreeIndexesInArray->size* sizeof(int),DISK_SIZE*sizeof(ArrayInfo_t**));
+}
+
+void array_deleteFromArray(int index)
+{
+    disk_mng_CB->arrayForAllMApsInformation[index] = NULL;
+}
+
+void array_deleteArrayInfo(ArrayInfo_t* arrayInfo)
+{
+    disk_deleteMap(arrayInfo->diskPointer);
+    free(arrayInfo);
+}
+
+
+
 
 void test_writeExceptionToFile(Exception exception, const char* source) {
     FILE* file;
--- Storage/Disk_Mng_Master.h
+++ Storage/Disk_Mng_Master.h
@@ -7,6 +7,9 @@
 
 // Forward declarations
 
+typedef struct Point_s Point_t;
+typedef struct MapRange_s MapRange_t;
+typedef struct ArrayInfo_s ArrayInfo_t;
 typedef struct StackNode_s StackNode_t;
 typedef struct DiskFreeIndexesInArray_s DiskFreeIndexesInArray_t;
 typedef struct Disk_Management_CB_s Disk_Managment_CB_t;
@@ -19,15 +22,36 @@ typedef enum {
 
 // Struct declarations
 
+struct Point_s {
+    int x; // longitude
+    int y; // latitude
+};
+
+struct MapRange_s {
+    Point_t topLeft; 
+    Point_t bottomRight; 
+};
+
+struct ArrayInfo_s {
+    int mapid;
+    int* diskPointer;
+    int size;
+    MapRange_t range;
+    //AVLNodeInfo_t  avlInfo;
+
+};
+
 struct StackNode_s {
     int freeIndex;
     StackNode_t* next;
 };
 
 struct DiskFreeIndexesInArray_s {
+    int size;
     StackNode_t* top;
 };
 struct Disk_Management_CB_s {
+    ArrayInfo_t** arrayForAllMApsInformation;
     StackNode_t* stackNode;
     DiskFreeIndexesInArray_t* diskFreeIndexesInArray;
 };
@@ -48,7 +72,9 @@ void disk_mng_initialize();
 
 //stack functions
 
-void stack_initialize();
+void stack_firstInitialize();
+
+void stack_normalInitialize();
 
 bool stack_is_empty();
 
@@ -59,4 +85,16 @@ StackNode_t* stackNode_create(int index);
 int stack_pop();
 
 int stack_top();
+
+//array functions 
+
+void array_normalInitialize();
+
+void array_firstInitialize();
+
+void array_deleteFromArray(int index);
+
+void array_deleteArrayInfo(ArrayInfo_t* arrayInfo);
+
+void disk_deleteMap(int* diskPointer);
 
