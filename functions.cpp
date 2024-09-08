924d16fdb41c8e9641c42ed4eacb4e3dc8bc2d18
NechamaRos <r0556744056@gmail.com>
2024-09-08 10:57:39 +0300

    Update functions.cpp
    now it work!!!

--- functions.cpp
+++ functions.cpp
@@ -91,7 +91,7 @@ bool myRunKernel(const vector<MPoint>v1, const vector<MPoint>v2, vector<double>&
 int checkIfBetter(vector<MPoint>v, vector<MPoint>v2, vector<double>H, const double defaultDistance)
 {
 	int countGoodMatch = 0;
-	//הצבה בהומוגרפיה
+	//Homography placement
 	for (int i = 0; i < v.size(); i++)
 	{
 		float ww = 1.f / (H[6] * v[i].x + H[7] * v[i].y + H[8]);
@@ -110,7 +110,6 @@ int checkIfBetter(vector<MPoint>v, vector<MPoint>v2, vector<double>H, const doub
 
 pair<double, double > calculateFunction(MPoint p, MPoint p2)
 {
-	//double Incline2= atan2(p2.y - p.y, p2.x - p.x) * 180 / 3.14159;;
 	double incline = (double(p2.y - p.y)) / (double(p2.x - p.x));
 	double b = p.y - p.x * incline;
 	return make_pair(incline, b);
@@ -132,39 +131,6 @@ bool checkIfOnSameLine(vector<MPoint>v)
 	}
 	return true;
 
-
-	/*MPoint p = v[randomNumbers[0]];
-	MPoint p2 = v2[randomNumbers[0]];
-	pair<double, double > incline = calculateFunction(p, p2);
-	pair<double, double > incline2;
-	for (int i = 1; i <= 3; i++)
-	{
-		p = v[randomNumbers[i]];
-		p2 = v2[randomNumbers[i]];
-		incline2 = calculateFunction(p, p2);
-		if (incline == incline2)
-			return true;
-	}
-	return false;*/
-	//
-	//MPoint p = v[randomNumbers[0]];
-	//MPoint p2 = v[randomNumbers[1]];
-	//MPoint p3 = v2[randomNumbers[0]];
-	//MPoint p4 = v2[randomNumbers[1]];
-	//pair<double, double > incline = calculateFunction(p, p2);
-	//pair<double, double > incline2 = calculateFunction(p3, p4);
-	//for (int i = 2; i <= 3; i++)
-	//{
-		//p = v[randomNumbers[i]];
-		//p2 = v2[randomNumbers[i]];
-		//int y = p.x * incline.first + incline.second;
-		//int y2 = p2.x * incline2.first + incline2.second;
-		//if (y == p.y || y2 == p2.y)
-			//return true;
-	//}
-	//return false;
-
-
 }
 
 bool myCheckSubset(const std::vector<MPoint>& ms1, const std::vector<MPoint>& ms2)
@@ -183,7 +149,6 @@ bool myCheckSubset(const std::vector<MPoint>& ms1, const std::vector<MPoint>& ms
 		double A = (src[t[0]].x, src[t[0]].y, 1., src[t[1]].x, src[t[1]].y, 1., src[t[2]].x, src[t[2]].y, 1.);
 		double B = (dst[t[0]].x, dst[t[0]].y, 1., dst[t[1]].x, dst[t[1]].y, 1., dst[t[2]].x, dst[t[2]].y, 1.);
 
-		//negative += determinant(A) * determinant(B) < 0;
 		negative += myDeterminant(&A) * myDeterminant(&B) < 0;
 	}
 	if (negative != 0 && negative != 4)
@@ -193,53 +158,11 @@ bool myCheckSubset(const std::vector<MPoint>& ms1, const std::vector<MPoint>& ms
 	return true;
 }
 
-
-//bool myCheckSubset(const std::vector<MPoint>& ms1, const std::vector<MPoint>& ms2)
-//{
-//	if (checkIfOnSameLine(ms1) || checkIfOnSameLine(ms2)) return false;
-//
-//
-//	static const int tt[][3] = { {0, 1, 2}, {1, 2, 3}, {0, 2, 3}, {0, 1, 3} };
-//	const MPoint* src = &ms1[0];// .ptr<MPoint>();
-//	const MPoint* dst = &ms2[0];// .ptr<MPoint>();
-//	int negative = 0;
-//
-//	for (int i = 0; i < 4; i++)
-//	{
-//		const int* t = tt[i];
-//		double A = (src[t[0]].x, src[t[0]].y, 1., src[t[1]].x, src[t[1]].y, 1., src[t[2]].x, src[t[2]].y, 1.);
-//		double B = (dst[t[0]].x, dst[t[0]].y, 1., dst[t[1]].x, dst[t[1]].y, 1., dst[t[2]].x, dst[t[2]].y, 1.);
-//
-//		//negative += determinant(A) * determinant(B) < 0;
-//		negative += myDeterminant(&A) * myDeterminant(&B) < 0;
-//	}
-//	if (negative != 0 && negative != 4)
-//		return false;
-//
-//
-//	return true;
-//}
-
 bool myGetSubset(const vector<MPoint> m1, const vector<MPoint> m2, vector<MPoint>& ms1, vector<MPoint>& ms2, int maxAttempts)
 {
-	//for iters from 1 to maxAttempts :
-		//randomly select modelPoints unique indices from 0 to length(m1) - 1
-		//	create empty arrays ms1 and ms2
-
-		//	for each selected index in the subset :
-		//copy the corresponding data point from m1 to ms1
-		//	copy the corresponding data point from m2 to ms2
-
-		//	if checkSubset(ms1, ms2) is true :
-		//		return true
-
-		//		return false
-
 	random_device rd;
 	mt19937 gen(rd());
 
-	//vector<MPoint> ms1;//(modelPoints);
-	//vector<MPoint> ms2;//(modelPoints);
 	for (int iters = 0; iters < maxAttempts; ++iters) {
 		vector<double> indices(m1.size());
 		iota(indices.begin(), indices.end(), 0);
@@ -258,31 +181,6 @@ bool myGetSubset(const vector<MPoint> m1, const vector<MPoint> m2, vector<MPoint
 			return true;
 		}
 	}
-	/* for( int iters = 0; iters < maxAttempts; ++iters )
-		{
-			int i;
-
-			for( i = 0; i < modelPoints; ++i )
-			{
-				int idx_i;
-
-				for ( idx_i = rng.uniform(0, count);
-					std::find(idx, idx + i, idx_i) != idx + i;
-					idx_i = rng.uniform(0, count) )
-				{}
-
-				idx[i] = idx_i;
-
-				for( int k = 0; k < esz1; ++k )
-					ms1ptr[i*esz1 + k] = m1ptr[idx_i*esz1 + k];
-
-				for( int k = 0; k < esz2; ++k )
-					ms2ptr[i*esz2 + k] = m2ptr[idx_i*esz2 + k];
-			}
-
-			if( cb->checkSubset(ms1, ms2, i) )
-				return true;
-		}*/
 
 	return false;
 }
@@ -314,30 +212,16 @@ vector<double>homography(vector<MPoint>v, vector<MPoint>v2, const double biggest
 
 	vector<MPoint>v1_randoMPoints;
 	vector<MPoint>v2_randoMPoints;
-	//vector<int> randomNumbers;
 	vector<double>currentHomography(9);
 	int count = sizeMPoints, biggestCount = 0;
 	for (int index = 0; index < 2000; index++)
 	{
 		v1_randoMPoints.clear();
 		v2_randoMPoints.clear();
-		//randomNumbers.clear();
-		//random_device rd;
-		//mt19937 gen(rd());
-		//uniform_int_distribution<> dis(0, v.size()-1);
 
-		//while (randomNumbers.size() < 4) {
-		//	int randomNumber = dis(gen);
-		//	if (std::find(randomNumbers.begin(), randomNumbers.end(), randomNumber) == randomNumbers.end()) {
-		//		randomNumbers.push_back(randomNumber);	
-		//		v1_randoMPoints.push_back(v.at(randomNumber));
-		//		v2_randoMPoints.push_back(v2.at(randomNumber));
-		//	}
-		//}
 		if (count > 4)
 		{
 			bool found = myGetSubset(v, v2, v1_randoMPoints, v2_randoMPoints, 10000);
-			//bool myGetSubset(const std::vector<MPoint>&m1, const std::vector<MPoint>&m2, int modelPoints, int maxAttempts)
 			if (!found)
 			{
 				if (index == 0)
@@ -348,16 +232,15 @@ vector<double>homography(vector<MPoint>v, vector<MPoint>v2, const double biggest
 				break;
 			}
 		}
-		//מציאת ההומוגרפיה בין 4 נקודות
+		//Finding the homography between 4 points
 		bool res = myRunKernel(v1_randoMPoints, v2_randoMPoints, currentHomography);
 		if (!res)
 			continue;
 
-		//בדיקה אם זו יותר טובה
+		//Check if this is better
 		count = checkIfBetter(v, v2, currentHomography, biggestDistance);
 		if (count > biggestCount)
 		{
-			/*cout << "count good points:  " << count << endl;*/
 			biggestCount = count;
 			mostGood = currentHomography;
 		}
@@ -365,10 +248,8 @@ vector<double>homography(vector<MPoint>v, vector<MPoint>v2, const double biggest
 	return mostGood;
 }
 
-double myDeterminant(const double* mat) {
-	/*  return a(0,0)*(a(1,1)*a(2,2) - a(2,1)*a(1,2)) -
-			a(0,1)*(a(1,0)*a(2,2) - a(2,0)*a(1,2)) +
-			a(0,2)*(a(1,0)*a(2,1) - a(2,0)*a(1,1));*/
+double myDeterminant(const double* mat)
+{
 	return mat[0] * (mat[4] * mat[8] - mat[5] * mat[7]) - 
 		mat[1] * (mat[3] * mat[8] - mat[5] * mat[6]) + 
 		mat[2] * (mat[3] * mat[7] - mat[4] * mat[6]);
