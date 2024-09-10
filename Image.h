130305dedb3afaf0576899552bb7ba749034d6a8
NechamaRos <r0556744056@gmail.com>
2024-09-10 14:35:30 +0300

    Update Image.h
    Added a note on each function

--- Image.h
+++ Image.h
@@ -60,51 +60,72 @@ public:
 
 	int total();
 
+	//return a tuple of the width and height
 	std::tuple<int, int> getSize();
 
 
+	//get a row, a col and a vector of three number- B,G,R- and set in this place this vector of pixel
 	void setAllPixel(int i, int j, const std::vector<uint8_t>& pixel);
 
+	//get a row, a col and number-0/1/2 and value - and set to specific channel in a pixel- 0-B, 1-G, 2-R the value
 	void setPixelCH(int i, int j, int c, int value);
 
 
+	//get a number of row and return a new Image of width*1 with the data of this row
 	Image row(int y) const;
 
+	//get a number of col and return a new Image of 1*height with the data of this col
 	Image col(int x) const;
 
+	//return true if the image is empty
 	bool empty();
 
+	//return new image by copy the data
 	Image clone()const;
 
+	//split the data of the image to three vector of B,G,R
 	std::vector<std::vector<uint8_t>>splitChannels()const;
 
+	//get three vectors - B,G,R and marge them together
 	static std::vector<uint8_t>margeChannels(std::vector<std::vector<uint8_t>>v);
 
+	//return the data of the image
 	std::vector<uint8_t>getData();
 
 
+	//returns the location of the row and column and the color sent
 	uint8_t at(int i, int j, int c);
 
+	//returns the location of the row and column sent
 	std::vector<uint8_t> at(int i, int j)const;
 
 
+	//pointer to start of a raw
 	uint8_t* ptr(int i);
 
+	//pointer to start of a pixel
 	uint8_t* ptr(int i, int j);
 
+	//pointer to specific channel in a pixel
 	uint8_t* ptr(int i, int j, int c);
 
-
-	Image operator+(Image const& other) const;
-
+	
+	//like copy c'tor
 	Image operator=(Image const& other);
 
+	//add to all the pixels the value
 	Image operator +(uint8_t value) const; // by element
 
+	//connect tow Image if match
+	Image operator+(Image const& other) const;
+
+	//sub to all the pixels the value
 	Image operator -(uint8_t value) const; // by element
 
+	// compare by element to other image
 	bool operator ==(Image const& other) const; // compare by element to other image
 
+	// compare all elements to value
 	bool operator ==(uint8_t const& value) const; // compare all elements to value
 
 };
