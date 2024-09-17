c7dc41db31ae3a6e2379b0ed2810bedd1c747655
micif <m05567220548@gmail.com>
2024-09-17 13:32:09 +0300

    Change the code to run with SYCL

--- Security/Security/ECC_impl.h
+++ Security/Security/ECC_impl.h
@@ -4,8 +4,12 @@
 #include "ECC.h"
 #include "Logger.h"
 #include "Utils.h"
-#define CHAR_BIT_ON 0xff;
+#include <CL/sycl.hpp>
+#include <vector>
+#include <stdexcept>
+
 using namespace std;
+using namespace sycl;
 
 // Function to encrypt data using ECC
 template<typename T>
@@ -22,9 +26,7 @@ EncryptionResult ECC::encrypt(const T* data, const Point& publicKey) const {
     }
     // Generate a random number k
     cpp_int k = generateSecureRandomNumber(1, curve.getN());
-    Logger::getInstance().log("encrypt: the point K is "+k.str());
-    //Logger::getInstance().log(k.str());
-
+    Logger::getInstance().log("encrypt: the point K is " + k.str());
 
     // Compute the points R and S
     Point R = curve.multiply(curve.getG(), k);
@@ -33,19 +35,46 @@ EncryptionResult ECC::encrypt(const T* data, const Point& publicKey) const {
     // Convert the x value of S to a vector of bytes
     cpp_int x_value = S.getX();
     vector<unsigned char> x_bytes;
+
     while (x_value > 0) {
         x_bytes.push_back(static_cast<unsigned char>(x_value % 256));
         x_value >>= 8;
     }
 
+    // Fill the remaining bytes with zeros if x_bytes is smaller than sizeof(T)
+    while (x_bytes.size() < sizeof(T)) {
+        x_bytes.push_back(0);
+    }
+
     // Encrypt the data using XOR with the bytes of x
     vector<unsigned char> ciphertext(sizeof(T));
     const unsigned char* dataBytes = reinterpret_cast<const unsigned char*>(data);
-    for (size_t i = 0; i < sizeof(T); ++i) {
-        ciphertext[i] = dataBytes[i] ^ x_bytes[i % x_bytes.size()];
-    }
 
-    return { ciphertext, R };
+    // SYCL queue
+    queue q;
+
+    // Buffers for dataBytes and ciphertext
+    buffer<unsigned char, 1> dataBytesBuf(dataBytes, range<1>(sizeof(T)));
+    buffer<unsigned char, 1> ciphertextBuf(ciphertext.data(), range<1>(sizeof(T)));
+    buffer<unsigned char, 1> xBytesBuf(x_bytes.data(), range<1>(x_bytes.size()));
+
+    q.submit([&](handler& h) {
+        auto dataAcc = dataBytesBuf.get_access<access::mode::read>(h);
+    auto cipherAcc = ciphertextBuf.get_access<access::mode::write>(h);
+    auto xBytesAcc = xBytesBuf.get_access<access::mode::read>(h);
+
+    h.parallel_for(range<1>(sizeof(T)), [=](id<1> i) {
+        cipherAcc[i] = dataAcc[i] ^ xBytesAcc[i % xBytesAcc.get_count()];
+        });
+        }).wait();
+
+        // Copy the results back to the ciphertext vector
+        auto ciphertextAcc = ciphertextBuf.get_access<access::mode::read>();
+        for (size_t i = 0; i < ciphertext.size(); ++i) {
+            ciphertext[i] = ciphertextAcc[i];
+        }
+
+        return { ciphertext, R };
 }
 
 // Function to decrypt data using ECC
@@ -60,18 +89,48 @@ T* ECC::decrypt(const EncryptionResult& encryptedMessage, cpp_int privateKey) co
     Point S = curve.multiply(encryptedMessage.R, privateKey);
 
     cpp_int x_value = S.getX();
- 
-    // Decrypt the data using XOR with the bytes of x
-    T* decryptedData = new T;
-     
-    unsigned char* decryptedDataBytes = reinterpret_cast<unsigned char*>(decryptedData);
-    for (size_t i = 0; i < sizeof(T); ++i) {
-        char mask = static_cast<unsigned char>(x_value) & CHAR_BIT_ON;
-        decryptedDataBytes[i] = encryptedMessage.ciphertext[i] ^ mask;
+
+    // Convert x_value to bytes
+    vector<unsigned char> x_bytes;
+    while (x_value > 0) {
+        x_bytes.push_back(static_cast<unsigned char>(x_value % 256));
         x_value >>= 8;
     }
 
-    return decryptedData;
+    // Fill the remaining bytes with zeros if x_bytes is smaller than sizeof(T)
+    while (x_bytes.size() < sizeof(T)) {
+        x_bytes.push_back(0);
+    }
+
+    // Decrypt the data using XOR with the bytes of x
+    T* decryptedData = new T;
+    unsigned char* decryptedDataBytes = reinterpret_cast<unsigned char*>(decryptedData);
+
+    // SYCL queue
+    queue q;
+
+    // Buffers for encryptedMessage.ciphertext and decryptedDataBytes
+    buffer<unsigned char, 1> cipherBuf(encryptedMessage.ciphertext.data(), range<1>(sizeof(T)));
+    buffer<unsigned char, 1> decryptedBuf(decryptedDataBytes, range<1>(sizeof(T)));
+    buffer<unsigned char, 1> xBytesBuf(x_bytes.data(), range<1>(x_bytes.size()));
+
+    q.submit([&](handler& h) {
+        auto cipherAcc = cipherBuf.get_access<access::mode::read>(h);
+    auto decryptedAcc = decryptedBuf.get_access<access::mode::write>(h);
+    auto xBytesAcc = xBytesBuf.get_access<access::mode::read>(h);
+
+    h.parallel_for(range<1>(sizeof(T)), [=](id<1> i) {
+        decryptedAcc[i] = cipherAcc[i] ^ xBytesAcc[i % xBytesAcc.get_count()];
+        });
+        }).wait();
+
+        // Copy the results back to the decrypted data
+        auto decryptedAcc = decryptedBuf.get_access<access::mode::read>();
+        for (size_t i = 0; i < sizeof(T); ++i) {
+            decryptedDataBytes[i] = decryptedAcc[i];
+        }
+
+        return decryptedData;
 }
 
 #endif // ECC_IMPL_H
